233837b4b127155ffe356c0b83785af8e257bfd6
JayasuryarajaSubburaj <jayasuryaraja1212@gmail.com>
2023-10-05 13:33:38 +0530

    file added

---
+++ src/main/java/com/onlinecompiler/controller/CompilerController.java
@@ -0,0 +1,36 @@
+package com.onlinecompiler.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.onlinecompiler.entity.CodeRequestEntity;
+import com.onlinecompiler.util.PythonRunner;
+
+@RestController
+@RequestMapping("/onlinelearning/")
+public class CompilerController {
+	
+	@PostMapping("/execute")
+	public String executeCode(@RequestBody CodeRequestEntity request) {
+		switch (request.getType()) {
+		case "python":
+			PythonRunner.runPythonCode();
+			break;
+		case "c":
+			// call the CRunner code here
+			break;
+		case "cpp":
+			// call the CppRunner code here
+			break;
+		case "java":
+			// call the JavaRunner code here
+			break;
+		default:
+			return "Invalid type!";
+		}
+		return "Execution finished!";
+	}
+
+}
---
+++ src/main/java/com/onlinecompiler/entity/CodeRequestEntity.java
@@ -0,0 +1,15 @@
+package com.onlinecompiler.entity;
+
+public class CodeRequestEntity {
+	
+	private String type;
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+}
